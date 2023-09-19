75a624dd3d663da147c0fdc7669c059fb0415370
Juliane Cassidy <julianecassidy@gmail.com>
2023-09-18 18:25:58 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `FetchFolio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
